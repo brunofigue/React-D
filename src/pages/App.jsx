324435ed721981beb6eb631fcaee516991edbbf8
bruno <brunofigueroa2290@gmail.com>
2024-09-30 19:39:46 -0300

    cambio ruta

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Layout from './Layout'
 import Home from "./Home"
 
+
 function App() {
   
 
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,6 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Footer from './Footer'
+import Header from './Header'
+
 
 const Layout = ({children}) => {
   return (
